a2535f8a9a3b43075d248a795776384ddfd7b4c9
Phillip Mitchell <4kphil.@Phillips-MacBook-Air.local>
2023-05-24 20:42:33 -0400

    video-player

---
+++ src/component/Videoplayer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import YouTube from 'react-youtube';
+
+const VideoPlayer = ({ video }) => {
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <div>
+      <YouTube videoId={video.id.videoId} opts={opts} />
+      <h2>{video.snippet.title}</h2>
+      <p>{video.snippet.description}</p>
+    </div>
+  );
+};
+
+export default VideoPlayer;
