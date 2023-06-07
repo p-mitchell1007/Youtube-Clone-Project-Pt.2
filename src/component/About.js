d95e849cd2279a2f5c47c81729211905dbf9488f
“irwinj7” <Irwinjorge@pursuit.com>
2023-06-06 22:06:35 -0400

    about updates

--- src/component/About.js
+++ src/component/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./About.css"
 import Steve_Irwin from "../Images/Steve_Irwin.jpg"
 import cr7 from "../Images/cr7.webp"
-import phil from "../Images/phil-4k.jpg"
+import phil from "../Images/philip_driver.jpeg"
 import jerry_mouse from "../Images/jerry-mouse.png"
 
 function About() {
@@ -12,25 +12,25 @@ function About() {
        <h3>Teammates</h3>
      <ul className='teammates'>
        <li className='teammate'>
-          <img className='' src={cr7} />
+          <img className='avatar' src={cr7} alt='picture-of-cristian'></img>
           <h4>Cristian</h4>
          <p>Fullstack Web Developer - Looking to change the world as a software engineer.</p>
           <a href="https://github.com/crsvalle">Teammate 1's GitHub</a>
       </li>
       <li className='teammate'>
-        <img className='' src={Steve_Irwin}></img>
+        <img className='avatar' src={Steve_Irwin} alt='picture-of-irwin'></img>
          <h4>Irwin</h4>
          <p>A passionate FSW dev in NYC</p>
            <a href="https://github.com/Irwinj7">Teammate 2's GitHub</a>
       </li>
       <li className='teammate'>
-         <img className='' src={jerry_mouse}></img>
+         <img className='avatar' src={jerry_mouse} alt='picture-of-jerry'></img>
          <h4>Jerry</h4>
          <p>Hello! I am currently in a course called Pursuit, studying full stack development. In my spare time, I love to watch anime and play metal music.</p>
            <a href="https://github.com/imjerryjohn">Teammate 3's GitHub</a>
       </li> 
       <li className='teammate'>
-         <img className='' src={phil}></img>
+         <img className='avatar' src={phil} alt='picture-of-phil'></img>
          <h4>Phillip</h4>
          <p>Full Stack Developer currently going through the modules of Pursuit. Passionate about elevating my career in tech and providing for my family.</p>
            <a href="https://github.com/p-mitchell1007">Teammate 4's GitHub</a>
