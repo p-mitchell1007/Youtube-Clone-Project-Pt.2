06f04e1b0ed425027ecc7f526b2f07ede2991e4b
“irwinj7” <Irwinjorge@pursuit.com>
2023-06-06 22:11:47 -0400

    about css tweaks

--- src/component/About.js
+++ src/component/About.js
@@ -13,24 +13,32 @@ function About() {
      <ul className='teammates'>
        <li className='teammate'>
           <img className='avatar' src={cr7} alt='picture-of-cristian'></img>
+          <br></br>
+          <br></br>
           <h4>Cristian</h4>
          <p>Fullstack Web Developer - Looking to change the world as a software engineer.</p>
           <a href="https://github.com/crsvalle">Teammate 1's GitHub</a>
       </li>
       <li className='teammate'>
         <img className='avatar' src={Steve_Irwin} alt='picture-of-irwin'></img>
+         <br></br>
+         <br></br>
          <h4>Irwin</h4>
          <p>A passionate FSW dev in NYC</p>
            <a href="https://github.com/Irwinj7">Teammate 2's GitHub</a>
       </li>
       <li className='teammate'>
          <img className='avatar' src={jerry_mouse} alt='picture-of-jerry'></img>
+         <br></br>
+         <br></br>
          <h4>Jerry</h4>
          <p>Hello! I am currently in a course called Pursuit, studying full stack development. In my spare time, I love to watch anime and play metal music.</p>
            <a href="https://github.com/imjerryjohn">Teammate 3's GitHub</a>
       </li> 
       <li className='teammate'>
          <img className='avatar' src={phil} alt='picture-of-phil'></img>
+         <br></br>
+         <br></br>
          <h4>Phillip</h4>
          <p>Full Stack Developer currently going through the modules of Pursuit. Passionate about elevating my career in tech and providing for my family.</p>
            <a href="https://github.com/p-mitchell1007">Teammate 4's GitHub</a>
